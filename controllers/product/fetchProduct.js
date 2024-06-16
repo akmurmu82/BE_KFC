ac523e79c77428450981b856303d9711db00fc8b
amit murmu <your.akmurmu6580@google.com>
2024-06-16 08:22:00 +0530

    added fetchProduct route

--- controllers/product/fetchProduct.js
+++ controllers/product/fetchProduct.js
@@ -3,19 +3,20 @@ const BE_BASE_URL = process.env.BE_KFC_BASE_URL;
 const { default: axios } = require("axios");
 const Product = require("../../models/productModel");
 
-const fetchAllProducts = async (req, res) => {
+const fetchProduct = async (req, res) => {
+  const { productId } = req.body;
   try {
-    const products = await Product.find();
+    const product = await Product.find({ _id:productId });
 
     // Respond with a success message
     res.status(200).json({
       status: true,
-      data: products,
+      data: product,
     });
   } catch (error) {
-    console.error("Error fetching products:", error);
+    console.error("Error fetching product:", error);
     res.status(500).json({ status: false, message: "Internal server error." });
   }
 };
 
-module.exports = fetchAllProducts;
+module.exports = fetchProduct;
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fetchAllProducts = require("../controllers/product/fetchAllProducts");
 const createProduct = require("../controllers/product/createProduct");
+const fetchProduct = require("../controllers/product/fetchProduct");
 require("dotenv").config();
 const productRouter = express.Router();
 
 productRouter.get("/all-products", fetchAllProducts);
+productRouter.post("/product", fetchProduct);
 productRouter.post("/create", createProduct);
 
 module.exports = productRouter;
